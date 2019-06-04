726af8b042ebea4a0c38c0783f0f4f70ea7eb304
Richard Huang <ziluoh@gmail.com>
2019-06-03 18:13:16 -0700

    update p2

---
+++ P2/problems/1_reverse_odd_range.js
@@ -0,0 +1,24 @@
+/*******************************************************************************
+Write a function reverseOddRange(start, end) that returns an array containing all
+odd numbers between 'start' and 'end'  in reverse-sequential order. Use a
+loop to do this.
+
+Examples:
+
+reverseOddRange(10, 20) => [ 19, 17, 15, 13, 11 ]
+reverseOddRange(3, 7) => [ 7, 5, 3 ]
+reverseOddRange(9, 5) => []
+*******************************************************************************/
+
+function reverseOddRange(start, end) {
+  var arr = [];
+  for(i = end; i >= start; i --){
+    if(i % 2 !== 0){
+      arr.push(i)
+    }
+  };
+  return arr;
+}
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*************************/
+module.exports = reverseOddRange;
